6e42f54af5c14fdcf70bec8080a3a74d99cc052e
khanhddq <suttoantruot13.11.2002@gmail.com>
2024-03-24 19:56:55 +0700

    booking

--- backend/src/routes/user.router.js
+++ backend/src/routes/user.router.js
@@ -1,27 +1,22 @@
 import express from 'express';
 import { userController } from '../controllers/index.js';
 import authJWT from '../middlewares/authJWT.js';
-import { body } from 'express-validator';
-import { validator } from '../middlewares/index.js'
+
 const userRouter = express.Router();
 
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 userRouter.post("/login", userController.login);
 // view list user
 
 // Get statstic user by role
 userRouter.get("/stastic-by-role", authJWT.verifyToken, userController.getListUserByRole);
 
-userRouter.get("/", [authJWT.verifyToken, authJWT.checkRole("Admin")], userController.FindAll);
+userRouter.get("/", userController.FindAll);
 // view user profile
 // created by: quốc khánh 
-userRouter.get("/:id", authJWT.verifyToken, userController.FindOne);
+userRouter.get("/:id", userController.FindOne);
 
 // Update User profile 
-userRouter.put("/:id", [authJWT.verifyToken,
-body("name").isLength({ max: 100 }).withMessage("Nam user can not exceed 100 words"),
-body("name").matches(emailRegex).withMessage("Invalid email format"),
-validator.checkError], userController.UpdateOne);
+userRouter.put("/:id", userController.UpdateOne);
 
 
 export default userRouter;
